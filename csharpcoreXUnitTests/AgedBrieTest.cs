e20a0157c8309ba366861a02d8b1774c1d9e9fda
Benoit <benoit.hoffman@epitech.eu>
2021-01-28 12:05:33 +0100

    TESTS - Added unit tests for Aged Brie

--- csharpcoreXUnitTests/AgedBrieTest.cs
+++ csharpcoreXUnitTests/AgedBrieTest.cs
@@ -1,41 +1,43 @@
-using csharpcore;
+﻿using csharpcore;
 using System;
 using System.Collections.Generic;
+using System.Text;
 using Xunit;
 
 namespace csharpcoreXUnitTests
 {
-    public class NormalItemsTest
+    public class AgedBrieTest
     {
         [Fact]
         public void Test()
         {
             IList<Item> items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 3, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
             };
 
             GildedRose gildedRose = new GildedRose(items);
 
             // normal update
             gildedRose.UpdateQuality();
-            Assert.Equal(2, items[0].SellIn);
-            Assert.Equal(19, items[0].Quality);
+            Assert.Equal(1, items[0].SellIn);
+            Assert.Equal(1, items[0].Quality);
 
             // reaches sell date
             gildedRose.UpdateQuality();
-            gildedRose.UpdateQuality();
             Assert.Equal(0, items[0].SellIn);
-            Assert.Equal(17, items[0].Quality);
+            Assert.Equal(2, items[0].Quality);
 
             // update after sell date has passed
             gildedRose.UpdateQuality();
             Assert.Equal(-1, items[0].SellIn);
-            Assert.Equal(15, items[0].Quality);
+            Assert.Equal(4, items[0].Quality);
 
-            // updated after Quality has reached 0
-            for (int i = 0; i != 10; i += 1)
+            //update after Quality has reached 50
+            for (int i = 0; i != 30; i += 1)
                 gildedRose.UpdateQuality();
-            Assert.Equal(0, items[0].Quality);
+            Assert.Equal(50, items[0].Quality);
+
+
         }
     }
 }
